f005d663da5c32c36a97c7f040ae04422b468ad5
yakovkhodorkovski <yaakov103@gmail.com>
2022-04-26 06:00:06 -0700

    more info added

---
+++ Game.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+#include <string>
+using namespace std;
+namespace coup{
+
+
+}
---
+++ Player.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+#include <string>
+using namespace std;
+namespace coup{
+    class Player { 
+        private : 
+        string player; 
+        public : 
+        Player(); 
+        ~Player();
+
+    };
+
+
+}
